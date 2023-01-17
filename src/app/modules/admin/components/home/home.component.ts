2ce67f7b2ff7cebac1a584267d8b9f7f25d24a7c
hafizasad45 <hafizasad45@gmail.com>
2023-01-17 21:38:22 +0600

    update mat table

--- src/app/modules/admin/components/home/home.component.ts
+++ src/app/modules/admin/components/home/home.component.ts
@@ -5,6 +5,7 @@ import { AfterViewInit, ViewChild } from '@angular/core';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ApiService } from 'src/app/services/api.service';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-home',
@@ -13,44 +14,35 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class HomeComponent implements OnInit {
   public users: any = [];
+  dataSource: any;
   displayedColumns: string[] = [
-    'id',
-    'password',
     'firstName',
     'lastName',
     'userName',
     'role',
     'email',
+    'action',
   ];
-  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private _liveAnnouncer: LiveAnnouncer, private api: ApiService) {}
+  constructor(private api: ApiService) {}
 
   ngOnInit() {
     this.api.getUsers().subscribe((res) => {
       this.users = res;
+      this.dataSource = new MatTableDataSource(this.users);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
-    this.dataSource = new MatTableDataSource(this.users);
-    console.log(this.dataSource);
   }
 
-  //@ViewChild(MatSort) sort: MatSort;
-  //@ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatSort) sort = new MatSort();
-
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
+  FilterChanged(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue;
   }
 
-  announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
-    if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
-    }
+  FunctionEdit(email: any) {
+    console.log(email);
   }
 }
