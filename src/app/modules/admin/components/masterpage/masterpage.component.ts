0bb78e83c2ec9e85346c7cfff90db78737098e19
ASAD <hafizasad45@gmail.com>
2023-02-19 23:20:04 +0600

    Menu Update

--- src/app/modules/admin/components/masterpage/masterpage.component.ts
+++ src/app/modules/admin/components/masterpage/masterpage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 import { ApiService } from 'src/app/services/api.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserStoreService } from 'src/app/services/user-store.service';
@@ -8,10 +9,10 @@ import { UserStoreService } from 'src/app/services/user-store.service';
   templateUrl: './masterpage.component.html',
   styleUrls: ['./masterpage.component.scss'],
 })
-export class MasterpageComponent implements OnInit {
-  opened = true;
 
-  public users: any = [];
+export class MasterpageComponent implements OnInit {
+
+    public users: any = [];
   public fullName: string = '';
   constructor(
     private authService: AuthService,
@@ -20,9 +21,9 @@ export class MasterpageComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    // this.api.getUsers().subscribe((res) => {
-    //   this.users = res;
-    // });
+     this.api.getUsers().subscribe((res) => {
+       this.users = res;
+     });
 
     this.userStote.getFullNameFromStore().subscribe((val) => {
       const fullNameFromToken = this.authService.getFullNameFromToken();
@@ -30,7 +31,58 @@ export class MasterpageComponent implements OnInit {
     });
   }
 
-  logout() {
-    this.authService.LogOut();
+  @ViewChild('sidenav') sidenav: MatSidenav | undefined;
+  isExpanded = true;
+  showSubmenu: boolean = false;
+  isShowing = false;
+  showSubSubMenu: boolean = false;
+
+  mouseenter() {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
   }
+
+  mouseleave() {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+  }
+
+  logout() {
+        this.authService.LogOut();
+      }
 }
+
+
+
+
+
+
+
+// export class MasterpageComponent implements OnInit {
+//   opened = true;
+
+//   public users: any = [];
+//   public fullName: string = '';
+//   constructor(
+//     private authService: AuthService,
+//     private api: ApiService,
+//     private userStote: UserStoreService
+//   ) {}
+
+//   ngOnInit() {
+//     // this.api.getUsers().subscribe((res) => {
+//     //   this.users = res;
+//     // });
+
+//     this.userStote.getFullNameFromStore().subscribe((val) => {
+//       const fullNameFromToken = this.authService.getFullNameFromToken();
+//       this.fullName = val || fullNameFromToken;
+//     });
+//   }
+
+//   logout() {
+//     this.authService.LogOut();
+//   }
+// }
